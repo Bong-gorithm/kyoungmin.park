6747c9cd8f1a6bdadcc3bb0914f496776d89430d
kymin <park48664866@gmail.com>
2026-06-03 16:15:25 +0900

    3주차 문제 풀이

---
+++ src/main/java/com/bongtu/string/LongestPalindrome.java
@@ -0,0 +1,33 @@
+package com.bongtu.string;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int answer = 0;
+        boolean hasOdd = false;
+        for (int i : map.values()) {
+
+            if (i % 2 == 0) {
+                answer += i;
+            } else {
+                answer += i - 1;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            answer++;
+        }
+
+        return answer;
+    }
+}
